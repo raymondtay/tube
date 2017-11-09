3641af3140c326b133866466082294e96d85ae63
Raymond Tay <raymondtay1974@gmail.com>
2017-11-09 16:21:40 +0800

    test

--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,13 +1,14 @@
 package nugit.tube
 
-import org.specs2._
+import org.specs2._    
 import org.scalacheck._
 import Gen.{containerOfN, choose, pick, mapOf, listOf, oneOf}
 import Prop.{forAll, throws, AnyOperators}
 
 class MainSpec extends mutable.Specification with TestTrait with ScalaCheck {
-  "this is a test" >> prop{ (xs: List[Int]) => 
-    validateSizeOfList(xs) 
-  }.set(minTestsOk = 200, workers = 1)
+  val  minimumNumberOfTests = 200
+  "this is a test" >> prop{ (xs: List[Int]) =>
+    validateSizeOfList(xs)
+  }.set(minTestsOk = minimumNumberOfTests, workers = 1)
 
 }
