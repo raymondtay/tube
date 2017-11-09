ec2e9dc2c77f99af2e8c1ad1dbc9f0a30c3cfa09
Raymond Tay <raymondtay1974@gmail.com>
2017-11-09 16:34:08 +0800

    Did a few things: (a) Experimented with git hooks to perform checks before the developer pushes the code (b) Installed 'overcommit' on dev environment to enable this experimentation (c) Created a document to show it can be done 'scalastyle.md'

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,11 +2,11 @@ package nugit.tube
 
 import cats._, data._, implicits._
 
-/** 
+/**
   * Dummy source file to test circle-ci
   */
 trait TestTrait  {
-  def validateSizeOfList[A](xs : List[A]) = xs match {
+  def validateSizeOfList[A](xs : List[A]) : Boolean = xs match {
     case Nil => 0 == 0
     case _ => xs.length  == xs.length
   }
