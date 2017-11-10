605870877bb6426e0bcc7ec8d7f380c74ec5eea6
Raymond Tay <raymondtay1974@gmail.com>
2017-11-10 12:50:25 +0800

    (a) Added circe-json to dependencies; (b) Added OpenTracing client libraries to dependencies (c) Added OpenTracing client-to-server library 'Jaeger' to dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,6 +8,9 @@ object Dependencies {
   val akkaVersion       = "2.5.6"
   val scalaCheckVersion = "1.13.4"
   val specs2Version     = "4.0.1"
+  val circeVersion      = "0.8.0"
+  val openTracingVersion = "0.30.0"
+  val jaegerCoreVersion  = "0.22.0-RC2"
 
   // Libraries
   val cats           = "org.typelevel" %% "cats-core" % catsVersion
@@ -18,9 +21,19 @@ object Dependencies {
   val specs2ScalaCheckTest = "org.specs2" %% "specs2-scalacheck" % specs2Version
   val akkaStream           = "com.typesafe.akka" %% "akka-stream" % akkaVersion
   val akkaActors           = "com.typesafe.akka" %% "akka-actor"  % akkaVersion
+  val circeJson            =  Seq(
+    "io.circe" %% "circe-core",
+    "io.circe" %% "circe-generic",
+    "io.circe" %% "circe-parser"
+    ).map(_ % circeVersion)
 
+  val openTracing = Seq(
+    "io.opentracing" % "opentracing-api" % openTracingVersion ,
+    "io.opentracing" % "opentracing-util" % openTracingVersion,
+    "com.uber.jaeger" % "jaeger-core" % jaegerCoreVersion)
+ 
   // Grouping the libraries to logical units
-  val generalLibs = Seq(cats, akkaHttp, akkaStream, akkaActors)
+  val generalLibs = Seq(cats, akkaHttp, akkaStream, akkaActors) ++ circeJson ++ openTracing
 
   val testLibs = Seq(akkaHttpTest, specs2ScalaCheckTest, specs2Test).map( _ % Test )
 
